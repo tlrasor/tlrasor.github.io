74bda504c9c0f0796e6632979fd6713efc2aba58
Travis Rasor <travis.rasor@argodata.com>
2016-07-17 16:45:25 -0500

    updating resume

--- serve_development.sh
+++ serve_development.sh
@@ -2,5 +2,5 @@
 LOGFILE=./build.log
 echo "$(date +%Y-%m-%d-%H:%M:%S) -- Serving project with DEVELOPMENT config. Check build.log for build details."
 echo "$(date +%Y-%m-%d-%H:%M:%S) -- Serving project with DEVELOPMENT config" >> $LOGFILE 2>&1
-bundler install >> LOGFILE 2>&1
+bundler install >> $LOGFILE 2>&1
 bundler exec jekyll serve --config _config.yml,_config_dev.yml >> $LOGFILE 2>&1
